45a6b92cf424e43133ec23f8f85caf13feda2661
Raineer <delaritaraineer81@gmail.com>
2017-11-28 17:57:30 +0800

    Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
-export const environment = {
-  production: true
+ export const environment = {
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyBizBM7tVm96YU2QXNg-uw7bBT2JD-922g",
+    authDomain: "fs1prod-1600a.firebaseapp.com",
+    databaseURL: "https://fs1prod-1600a.firebaseio.com",
+    projectId: "fs1prod-1600a",
+    storageBucket: "fs1prod-1600a.appspot.com",
+    messagingSenderId: "25066471255"
+},
 };
